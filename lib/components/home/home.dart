56c726111e73ea0d68c25e2ced156d0012174009
arnu515 <arnu515@protonmail.com>
2022-01-11 20:58:34 +0530

    feat: ability to add applications

--- lib/components/home/home.dart
+++ lib/components/home/home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter/rendering.dart';
 import 'package:simpleauthenticator/models/application.dart';
 
 class Home extends StatefulWidget {
@@ -10,12 +11,71 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   List<Application> apps = Application.fetchAll();
+  final GlobalKey<FormState> _addAppFormKey = GlobalKey<FormState>();
   
   _deleteApp(String id) {
     setState(() {
       apps.removeWhere((app) => app.id == id);
       apps = apps;
     });
+  }
+
+  Widget createAddModal(BuildContext context) {
+    String? enteredName, enteredKey;
+
+    return SizedBox(
+      height: 350,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Add application", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+            Form(
+              key: _addAppFormKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(padding: EdgeInsets.all(12.0)),
+                  const Text("Name", style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500)),
+                  const Padding(padding: EdgeInsets.all(2.0)),
+                  TextFormField(
+                    onSaved: (String? value) => enteredName = value,
+                    decoration: const InputDecoration(hintText: "Enter the application's name"),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) return "Enter a value";
+                      return null;
+                    }
+                  ),
+                  const Padding(padding: EdgeInsets.all(4.0)),
+                  const Text("App key", style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500)),
+                  TextFormField(
+                    onSaved: (String? value) => enteredKey = value,
+                    decoration: const InputDecoration(hintText: "Enter TOTP Key"),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) return "Enter a value";
+                      return null;
+                    }
+                  ),
+                  const Padding(padding: EdgeInsets.all(4.0)),
+                  ElevatedButton(onPressed: () {
+                    if (!_addAppFormKey.currentState!.validate()) return;
+                    _addAppFormKey.currentState!.save();
+                    if (enteredKey == null || enteredName == null) return;
+                    setState(() {
+                      apps.add(Application("3", enteredName!, enteredKey!));
+                    });
+                    Navigator.pop(context);
+                  }, child: const Padding(padding: EdgeInsets.symmetric(vertical: 8.0), child: Text("Add application", style: TextStyle(fontSize: 18.0))))
+                ]
+              )
+            )
+          ]
+        )
+      )
+    );
   }
 
   @override
@@ -49,7 +109,9 @@ class HomeState extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {  },
+        onPressed: () { 
+          showModalBottomSheet(context: context, builder: createAddModal);
+        },
         tooltip: "Add application",
       ),
     );
